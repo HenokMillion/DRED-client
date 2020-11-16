d461551253d750bcbe9a7bb3dfeab971f200f58a
Henok Million <henokmillionmekuria@gmail.com>
2020-11-16 23:48:55 +0300

    login: progress indicator, stateful component

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,60 +4,92 @@ import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import Img from "gatsby-image"
 import Paper from '@material-ui/core/Paper';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 import SEO from "../components/seo"
 import { Image } from "../components/image"
 import { handleLogin } from '../services/auth'
 
-let user =  {
-    username: "",
-    password: ""
-}
-const login = (e) => {
-    e.preventDefault();
-    // console.log(user)
-    handleLogin(user)
-}
+class Login extends React.Component {
 
-const Login = () => (
-    <Container style={{ display: 'flex', justifyContent: "center", alignItems: "center", backgroundColor: '#E4DFED', width: '100vw', height: '100vh', margin: '0', maxWidth: 'unset' }}>
-        {/* <Image type="left" /> */}
-        <Paper style={{ width: '60vw', height: '80vh' }}>
-            <Container>
-                <SEO title="Login" />
-                <Grid container spacing={1} item>
-                    <Grid item lg={6}>
-                        {/* <Image type="left" /> */}
-                    </Grid>
-                    <Grid item lg={6} style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-                        <h1>Login</h1>
-                        <Grid item container>
-                            <Image type="left" />
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggingIn: false,
+            user: { username: '', password: '' }
+        }
+    }
+
+    renderProgress() {
+        return this.state.loggingIn ? (
+            <span>
+                <LinearProgress />
+                <Paper style={{
+                    width: '100%', height: '100%',
+                    zIndex: '9', backgroundColor: '#e0e0e0', position: 'absolute', opacity: '.3'
+                }}></Paper>
+            </span>
+        ) : null
+    }
+
+    async login(e) {
+        e.preventDefault();
+        console.log(this.state.user)
+        const loginSuccess = await handleLogin(this.state.user)
+        this.setState({loggingIn: true})
+        setTimeout(() => {
+            if (loginSuccess) {
+                //
+                console.log('LOGIN SUCCESS', loginSuccess)
+            } else {
+                //
+                console.log('LOGIN FAILED', loginSuccess)
+            }
+            this.setState({loggingIn: false})
+        }, 5000)
+    }
+
+    render() {
+        return (
+            <Container style={{ display: 'flex', justifyContent: "center", alignItems: "center", backgroundColor: '#E4DFED', width: '100vw', height: '100vh', margin: '0', maxWidth: 'unset' }}>
+                {/* <Image type="left" /> */}
+                <Paper style={{ width: '60vw', height: '80vh', position: 'relative' }}>
+                    {this.renderProgress()}
+                    <Container>
+                        <SEO title="Login" />
+                        <Grid container spacing={1} item>
+                            <Grid item lg={6}>
+                            </Grid>
+                            <Grid item lg={6} style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+                                <h1>Login</h1>
+                                <Grid item container>
+                                    <Image type="loginimg" />
+                                </Grid>
+                                <form onSubmit={e => this.login(e)}>
+                                    <Grid item container>
+                                        <TextField disabled={this.state.loggingIn} onChange={e => this.setState({user: { username: e.target.value, password: this.state.user.password }})} id="username" label="Username" variant="outlined" />
+                                    </Grid>
+                                    <Grid item container>
+                                        <TextField disabled={this.state.loggingIn} onChange={e => this.setState({user: { username: this.state.user.username, password: e.target.value }})} id="password" label="Password" type="password" variant="outlined" />
+                                    </Grid>
+                                    <Grid item container>
+                                        <Link to="#">Forgot Password?</Link>
+                                    </Grid>
+                                    <Grid item container>
+                                        <Button type="submit" disabled={this.state.loggingIn} variant="contained" color="primary">
+                                            SIGN IN
+                                        </Button>
+                                    </Grid>
+                                </form>
+                            </Grid>
                         </Grid>
-                        <form onSubmit={e => login(e)}>
-                            <Grid item container>
-                                <TextField onChange={e => {user.username = e.target.value}} id="username" label="Username" variant="outlined" />
-                            </Grid>
-                            <Grid item container>
-                                <TextField onChange={e => {user.password = e.target.value}} id="password" label="Password" type="password" variant="outlined" />
-                            </Grid>
-                            <Grid item container>
-                                <Link to="/">Go back to the homepage</Link>
-                            </Grid>
-                            <Grid item container>
-                                <Button type="submit" variant="contained" color="primary">
-                                    SIGN IN
-                        </Button>
-                            </Grid>
-                        </form>
-                    </Grid>
-                </Grid>
+                    </Container>
+                </Paper>
             </Container>
-        </Paper>
-    </Container>
-)
+        )
+    }
+}
 
 
 export default Login
